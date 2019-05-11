dcb8d13c4c56cb3fb6b0374d860c4dce38c05cac
Hi King <nokia6102@gmail.com>
2019-05-11 14:40:18 +0800

    Fix : Intro Dot don't move ,than Intro page dot move now.

--- BＭIBattle/ViewController.swift
+++ BＭIBattle/ViewController.swift
@@ -17,8 +17,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let pageViewController = segue.destination as? PageViewController {
+            
+            // 代理 pageViewController
+            pageViewController.pageViewControllerDelegate = self
+        }
+    }
+    
+}
 
+extension ViewController: PageViewControllerDelegate {
+    
+    /// 設定總頁數
+    ///
+    /// - Parameters:
+    ///   - pageViewController: _
+    ///   - numberOfPage: _
+    func pageViewController(_ pageViewController: PageViewController, didUpdateNumberOfPage numberOfPage: Int) {
+        self.pageControl.numberOfPages = numberOfPage
+    }
+    
+    /// 設定切換至第幾頁
+    ///
+    /// - Parameters:
+    ///   - pageViewController: _
+    ///   - pageIndex: _
+    func pageViewController(_ pageViewController: PageViewController, didUpdatePageIndex pageIndex: Int) {
+        self.pageControl.currentPage = pageIndex
+    }
+
+    
 }
 
