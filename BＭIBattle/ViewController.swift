e2d866e7b17c2e0013342efbc58d1c5ea1cb908f
user <user@users-Mac.local>
2019-05-11 12:56:30 +0800

    Initial Commit

---
+++ BＭIBattle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BＭIBattle
+//
+//  Created by user on 2019/5/11.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
