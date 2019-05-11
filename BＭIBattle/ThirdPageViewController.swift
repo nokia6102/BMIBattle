652ed2694015accddd371bfa731ca7e4311f2c77
Hi King <nokia6102@gmail.com>
2019-05-11 13:41:06 +0800

    First Commit! Intro page 3 OK.

---
+++ BＭIBattle/ThirdPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdPageViewController.swift
+//  BＭIBattle
+//
+//  Created by user on 2019/5/11.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ThirdPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
