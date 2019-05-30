2d23f446fb66a82d972b255928a801568ef9d563
Hi King <nokia6102@gmail.com>
2019-05-30 20:48:04 +0800

    第一個Tabs好友Table和cell UI設計加入

---
+++ BＭIBattle/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  BＭIBattle
+//
+//  Created by user on 2019/5/30.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
